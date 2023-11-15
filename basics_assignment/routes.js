854457dce55c9e0bdcf15372cdf0b6c2e6416cdf
SebastianMayorgaCaleron <sebasmayorga7@gmail.com>
2023-11-14 19:34:50 -0600

    basics

---
+++ basics_assignment/routes.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/users") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write("<body><ul><li> user 1</li> <li> user 2</li></ul></body>");
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/create-user" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      console.log(message);
+    });
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
+};
+
+// module.exports = requestHandler;
+
+// module.exports = {
+//     handler: requestHandler,
+//     someText: 'Some hard coded text'
+// };
+
+// module.exports.handler = requestHandler;
+// module.exports.someText = 'Some text';
+
+exports.handler = requestHandler;
+exports.someText = "Some hard coded text";
